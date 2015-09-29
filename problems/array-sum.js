0f56124ac2567029fc71f0da7d4afe39cb980589
Tanner Schreyer <tanner.schreyer@gmail.com>
2015-09-29 08:35:04 -0600

    quiz2 done

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function arraySum(arr) {
+  var total = 0;
 
+  for (var i = 0; i < arr.length; i++){
+    var element = arr[i];
+    if (typeof element === 'object'){
+      total += arraySum(element);
+    } else {
+      total = total + element;
+    }
+  }
+
+  return total;
 }
-
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,28 @@
 'use strict';
 
 function longestString(arr) {
+  var lgth = 0;
+  var longest;
 
+
+  for(var i=0; i < arr.length; i++){
+    var elem = arr[i]
+    if(typeof elem === 'string'){
+         if (!longest || elem.length > longest.length){
+        longest = elem;
+    }
 }
+}
+return longest;
+}
+
+
+// return arr.sort(function (a, b) {
+//   if (typeof a !== 'string' || typeof b !== 'string'){return 0; }
+//   if (a.length > b.length) {retrun 1; }
+//   if (b.length > a.length) {retrun -1; }
+//   return 0;
+// })[0];
+
 
 module.exports = longestString;
